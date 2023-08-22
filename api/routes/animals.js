d5e82c453eb7c8dd93be21e916d6940d0e5ee127
orshemesh1996 <72469925+orshemesh1996@users.noreply.github.com>
2023-08-22 13:28:53 +0300

    add swagger

--- api/routes/animals.js
+++ api/routes/animals.js
@@ -16,12 +16,126 @@ const {
 *   name: Animals 
 *   description: the Animals api 
 */
+/**
+* @swagger
+* components:
+*   schemas:
+*     Animals:
+*       type: object
+*       required:
+*         - message
+*         - sender
+*       properties:
+*         id:
+*           type: id
+*           description: The id of animal
+*         name:
+*           type: string
+*           description: The name of animal
+*         age:
+*           type: int
+*           description: The id of take animal
+*         animalStatus:
+*           type: string
+*           description: The animal status
+*         userId:
+*           type: id
+*           description: the owner of animal
+*         userEffortId:
+*           type: id
+*           description: the effort id of animal
+*       example:
+*         id: '1234'
+*         name: 'yossi'
+*         age: '15'
+*         animalStatus: '4566'
+*         userId: 'fffff'
+*         userEffortId: 'ff'
+*/
 
-
+/**
+* @swagger
+* /animals:
+*   get:
+*     summary: get all Animals
+*     tags: [Animals]
+*     responses:
+*       200:
+*         description: The get list of Animals
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.get('/',getAllAnimals);
+/**
+* @swagger
+* /animalId:
+*   get:
+*     summary: get one Animal
+*     tags: [Animals]
+*     responses:
+*       200:
+*         description: The get one Animals
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.get('/:animalId',getAnimal);
-
+/**
+* @swagger
+* /animals:
+*   post:
+*     summary: add a new Animal
+*     tags: [Animals]
+*     responses:
+*       200:
+*         description: add a new Animal
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.post('/',checkAuth,upload.single('image'),createAnimal);
+/**
+* @swagger
+* /animalId:
+*   patch:
+*     summary: update Animal
+*     tags: [Animals]
+*     responses:
+*       200:
+*         description: update Animal
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.patch('/:animalId',checkAuth,updateAnimal);
+/**
+* @swagger
+* /animalId:
+*   delete:
+*     summary: delete Animal
+*     tags: [Animals]
+*     responses:
+*       200:
+*         description: delete Animal
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.delete('/:animalId',checkAuth, deleteAnimal);
 module.exports = router;
--- api/routes/requests.js
+++ api/routes/requests.js
@@ -15,11 +15,119 @@ const {
 *   name: Requests 
 *   description: the Requests api 
 */
+/**
+* @swagger
+* components:
+*   schemas:
+*     Requests:
+*       type: object
+*       required:
+*         - message
+*         - sender
+*       properties:
+*         id:
+*           type: id
+*           description: The id of Request
+*         animalId:
+*           type: id
+*           description: The email of animal
+*         userId:
+*           type: id
+*           description: The id of take user
+*         contant:
+*           type: string
+*           description: The contant
+*       example:
+*         name: 'this is swagger test message'
+*         email: '123456@gmail.com'
+*         userType: 'EFFORT'
+*         password: '4566'
+*         animalId: 'fffff'
+*/
 
+/**
+* @swagger
+* /requests:
+*   get:
+*     summary: get all requests
+*     tags: [Requests]
+*     responses:
+*       200:
+*         description: The get list of requests
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.get('/',getAllRequest);
+/**
+* @swagger
+* /requestId:
+*   get:
+*     summary: get request
+*     tags: [Requests]
+*     responses:
+*       200:
+*         description: The get request
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.get('/:requestId',getRequest);
-
+/**
+* @swagger
+* /request:
+*   post:
+*     summary: create request
+*     tags: [Requests]
+*     responses:
+*       200:
+*         description: The create request
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.post('/',createRequest);
+/**
+* @swagger
+* /requestId:
+*   patch:
+*     summary: update request
+*     tags: [Requests]
+*     responses:
+*       200:
+*         description: The update request
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.patch('/:requestId',updateRequest);
+/**
+* @swagger
+* /requestId:
+*   delete:
+*     summary: delete request
+*     tags: [Requests]
+*     responses:
+*       200:
+*         description: The delete request
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/User'
+*/
 router.delete('/:requestId', deleteRequest);
 module.exports = router;
