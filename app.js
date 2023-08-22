946f59d20b88ea2730fdbc5163e2a24018106224
orshemesh1996 <72469925+orshemesh1996@users.noreply.github.com>
2023-08-22 17:24:27 +0300

    add swagger

--- app.js
+++ app.js
@@ -3,37 +3,40 @@ require('dotenv').config();
 //mongodb://127.0.0.1:27017/animalAdoption
 //onlz8n9uG57qf1cU
 const app = express();
-const mongoose =require('mongoose');
-const animalesRoutes=require('./api/routes/animals');
-const usersRoutes=require('./api/routes/users');
-const requestRoutes=require('./api/routes/requests');
+const mongoose = require('mongoose');
+const animalesRoutes = require('./api/routes/animals');
+const usersRoutes = require('./api/routes/users');
+const requestRoutes = require('./api/routes/requests');
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
-if (process.env.NODE_ENV == "development"){
-     const swaggerJSDoc=require('swagger-jsdoc')
-     const swaggerUi=require('swagger-ui-express')
-     const options ={ 
-          definition : { 
-              openapi: '3.0.0',
-               info : {
-                 title : " Api animals project",
-                 version : '1.0.0'
-                 },
-                    servers:[
-                          {
-                               api :'http://localhost:3000/'
-                                }
-                            ]
-                         },
-                         apis: ['./api/routes/*.js']
-                        }
-const swaggerSpec=swaggerJSDoc(options)
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
+// if (process.env.NODE_ENV == "development"){
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: " Api animals project",
+            version: '1.0.0'
+        },
+        servers: [
+            {
+                api: 'http://localhost:3000/'
+            },
+            {
+                api: 'https://node-mongodb-api-5dig.onrender.com/'
+            }
+        ]
+    },
+    apis: ['./api/routes/*.js']
 }
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+// }
 
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Database connected successfully!'))
     .catch(() => console.log('Unable to connect to database'));
 
@@ -50,10 +53,10 @@ app.use((req, res, next) => {
 //routes
 
 
-app.use('/animals',animalesRoutes);
-app.use('/users',usersRoutes);
-app.use('/requests',requestRoutes);
-app.use('/upload',express.static('uploads'))
+app.use('/animals', animalesRoutes);
+app.use('/users', usersRoutes);
+app.use('/requests', requestRoutes);
+app.use('/upload', express.static('uploads'))
 app.use((req, res, next) => {
     const error = new Error('Not Found');
     error.status = 404;
